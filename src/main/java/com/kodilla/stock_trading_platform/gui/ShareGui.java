5b1b434fd06c6071840936654d8548399da3a8ac
andzia0520 <ankadaniluk@gmail.com>
2021-03-11 12:09:04 +0100

    shareGui

---
+++ src/main/java/com/kodilla/stock_trading_platform/gui/ShareGui.java
@@ -0,0 +1,49 @@
+package com.kodilla.stock_trading_platform.gui;
+
+import com.kodilla.stock_trading_platform.webClient.stockExchange.StockExchangeClient;
+import com.kodilla.stock_trading_platform.webClient.stockExchange.StockExchangeController;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Route(value = "share")
+public class ShareGui extends VerticalLayout {
+    @Autowired
+    private StockExchangeClient stockExchangeClient;
+
+    @Autowired
+    private StockExchangeController stockExchangeController;
+
+    private TextField textFieldCompanyName;
+    private Button checkSymbolButton;
+    private Label labelShareSymbol;
+    private TextField textFieldShareSymol;
+    private Button checkPriceButton;
+    private Label labelPrice;
+
+    public ShareGui() {
+        textFieldCompanyName = new TextField("Put full company name");
+        checkSymbolButton = new Button("Show share symbol", new Icon(VaadinIcon.TRENDING_UP));
+        labelShareSymbol = new Label();
+        textFieldShareSymol = new TextField("Put share symbol to check price");
+        checkPriceButton = new Button("Check price", new Icon(VaadinIcon.DOLLAR));
+        labelPrice = new Label();
+
+        checkSymbolButton.addClickListener(e -> {
+            labelShareSymbol.setText("Share symbol of " + textFieldCompanyName.getValue() + " is " + stockExchangeController.getShareSymbol(textFieldCompanyName.getValue()));
+        });
+
+        checkPriceButton.addClickListener(e -> {
+            labelPrice.setText(stockExchangeController.getSharePrice(textFieldShareSymol.getValue()).toString());
+        });
+
+        add(textFieldCompanyName, checkSymbolButton,labelShareSymbol,textFieldShareSymol,checkPriceButton,labelPrice);
+
+        setSizeFull();
+    }
+}
--- src/main/java/com/kodilla/stock_trading_platform/webClient/stockExchange/StockExchangeController.java
+++ src/main/java/com/kodilla/stock_trading_platform/webClient/stockExchange/StockExchangeController.java
@@ -1,9 +1,9 @@
 package com.kodilla.stock_trading_platform.webClient.stockExchange;
 
-//import com.kodilla.stock_trading_platform.externalData.stockExchangeApi.client.StockExchangeClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,12 +14,12 @@ public class StockExchangeController {
     private final StockExchangeService stockExchangeService;
 
     @RequestMapping(method = RequestMethod.GET, value = "getSharePrice")
-    public SharePriceResponse getSharePrice() {
-        return stockExchangeService.getSharePrice();
+    public SharePriceResponse getSharePrice(@RequestParam String shareSymbol) {
+        return stockExchangeService.getSharePrice(shareSymbol);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getShareSymbol")
-    public ShareSymbolResponse getShareSymbol() {
-        return stockExchangeService.getShareSymbolByName();
+    public ShareSymbolResponse getShareSymbol(@RequestParam String companyName) {
+        return stockExchangeService.getShareSymbolByName(companyName);
     }
 }
